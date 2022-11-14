66430a71e1ccddab25ee228e8355b0162db66178
TimmyYoung <77657486+TimmyYoung@users.noreply.github.com>
2022-11-14 12:12:32 -0800

    Fixed Root display Ex. #2-Sessions
    Fixed bug where root directory would not display!! This was driving me crazy lol

--- hw4/02-sessions.js
+++ hw4/02-sessions.js
@@ -6,48 +6,30 @@ const port = process.env.PORT || 5001;
 
 // Use the express-session module
 //Save uninitialized and resave do not change output for this exercise. Recommended defaults are false. 
-app.use(session({secret: "42", saveUninitialized: true, resave: true, store: new session.MemoryStore()}));
-//Calling express middleware for static files. Saving the session details staticly via the public folder.
-app.use(express.static('public'));
+app.use(session({secret: "42", saveUninitialized: false, resave: false, store: new session.MemoryStore()}));
 //Calling express middleware for parsing the url data via querystring library.
 app.use(express.urlencoded({ extended: false }));
+home = true;
 
-//For some reason I cannot get any display to work for root....
-//Works for vscode debugger but not for command line..
-app.get('/', (req,res) =>{
-    let display  = "Currently on route: " + req.url + "<BR><BR>" + 
-        "Welcome to " + req.protocol + "://" + req.get("host") + req.url;
-    res.send(display);
-    res.end();
-    //next();
-});
-
-//Display works properly in vscode debugger.
+//Fixed the display by using originalURL and session.route.
 //Must make another request after root to get any output via command line..
-app.use('/', (req, res) => {
-    if (req.session.routesVisited) {
-        let display  = "Currently on route: " + req.url +
-        "<BR> <BR> Previously visited: <BR>";
-        req.session.routesVisited.forEach(route => {
-            display = display.concat("<BR>" + route);  
-            });
-        req.session.routesVisited.push(req.url);
-        res.send(display);
-        res.end();
+app.get('*', (req, res) => {
+    var display;
+    if (home) {
+        display  = "Currently on route: " + req.originalUrl + "<BR>";
+        display += "Welcome to " + req.protocol + " ://" + req.get("host") + req.originalUrl;
+        home = false;
+        req.session.routesVisited = [];
+        req.session.routesVisited.push('/');
     } 
     else {
-        req.session.routesVisited = [];
-        let display  = "Currently on route: " + req.url + "<BR><BR>" + 
-        "Welcome to " + req.protocol + "://" + req.get("host") + req.url;
-        req.session.routesVisited.push('/');
-        req.session.routesVisited.push(req.url);
-        res.send(display);
-        res.end();
-  }
-});
-
-//Making a post request/response.
-app.post('', (req, res) => {
+        display  = "Currently on route: " + req.originalUrl +"<BR> <BR> Previously visited: <BR>";
+        req.session.routesVisited.push(req.originalUrl);
+    }
+    for(urls in req.session.routesVisited){
+        display += "<BR>" + req.session.routesVisited[urls];
+        }
+    res.send(display);
 });
 
 //Creating the server from the port given above.
